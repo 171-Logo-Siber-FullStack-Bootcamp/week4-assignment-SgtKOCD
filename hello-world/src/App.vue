c71d7036a81932bbc8ec3d5c3159ca9f0bcf1811
Kağan Demiröz <e3_kaan@icloud.com>
2022-03-02 15:52:38 +0300

    up

--- hello-world/src/App.vue
+++ hello-world/src/App.vue
@@ -8,22 +8,27 @@
         </div>
       </div>
     </div>
+    <a class="scroll-to-top rounded" href="#page-top">
+      <i class="fas fa-angle-up"></i>
+    </a>
+    <LogOut />
   </div>
 </template>
 
 <script>
 import SideBar from "./components/SideBar/SideBar.vue";
 import TopBar from "./components/TopBar/TopBar.vue";
+import LogOut from "./components/LogOut/LogOut.vue";
 export default {
   name: "App",
 
   components: {
     SideBar,
-    TopBar
+    TopBar,
+    LogOut,
   },
 };
 </script>
 
 <style>
-
 </style>
